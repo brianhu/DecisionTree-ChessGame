d2523fe5cab62cc48db27dbd7c0637dedee1ef08
Brian Hu <billy170916@gmail.com>
2012-12-06 16:54:08 +0800

    map can be initialized well now && separated map from main.py

--- main.py
+++ main.py
@@ -1,73 +1,18 @@
-import ConfigParser
+from map import Map
 import pygame, sys
 from pygame.locals import *
-
-
-class Map(object):
-    def load_map(self, filename='level.map'):
-        self.map = []
-        self.key = {}
-        parser = ConfigParser.ConfigParser()
-        parser.read(filename)
-        self.tileset = parser.get('level', 'tileset')
-        self.map = parser.get('level', 'map').split("\n")
-        for section in parser.sections():
-            if len(section) == 1:
-                desc = dict(parser.items(section))
-                self.key[section] = desc
-
-        self.width = len(self.map[0])
-        self.height = len(self.map)
-        return self.map
-
-    def get_info(self, x, y):
-        """
-            this method is used to get info of a specific grid on map
-            a map like:
-            ...
-            .#.
-            ##.
-            it's a list: ['...','.#.','##.'] in python
-            map[0][1] is .
-            map[1][1] is #
-        """
-        try:
-            char = self.map[x][y]
-        except IndexError:
-            return {}
-        try:
-            return self.key[char]
-        except KeyError:
-            return {}
-
-    def is_enamy(self, x, y, camp):
-        """
-            this method is used to check whether there is an enamy
-            on a specific grid.
-        """
-        enamy_map = {
-            'human' : 'computer',
-            'computer' : 'human'
-        }
-        grid_info = self.get_info(x, y)
-        try:
-            return grid_info['camp'] == enamy_map[camp]
-        except KeyError:
-            return False
-
 
 BLUE = (0, 0, 128)
 GREEN = (0, 255, 0)
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
+screen = pygame.display.set_mode((400, 500))
 pygame.display.set_caption('Hello World!')
-fontObj = pygame.font.Font('freesansbold.ttf', 32)
+fontObj = pygame.font.Font('MONACO.ttf', 32)
 map = Map()
-map = map.load_map()
 x_position = 200
-y_position = 10
+y_position = 40
 
-for row in map:
+for row in map.map:
     textSurfaceObj = fontObj.render(row, True, GREEN, BLUE)
     textRectObj = textSurfaceObj.get_rect()
     textRectObj.center = (x_position, y_position)
---
+++ map.py
@@ -0,0 +1,85 @@
+import ConfigParser
+from random import randint
+
+
+class Map(object):
+    def __init__(self, filename='level.map'):
+        self.map = []
+        human_agent_list = ['G','A','C','I','I']
+        computer_agent_list = ['g','a','c','i','i']
+        self.key = {}
+        parser = ConfigParser.ConfigParser()
+        parser.read(filename)
+        self.map = parser.get('level', 'map').split("\n")
+        for section in parser.sections():
+            if len(section) == 1:
+                desc = dict(parser.items(section))
+                self.key[section] = desc
+
+        for agent in human_agent_list:
+            x = randint(0,4)
+            y = randint(0,4)
+            if self.map[x][y] == '.':
+                buffer = list(self.map[x])
+                buffer[y] = agent
+                buffer = "".join(buffer)
+                self.map[x] = buffer
+
+        for agent in computer_agent_list:
+            x = randint(5,9)
+            y = randint(5,9)
+            if self.map[x][y] == '.':
+                buffer = list(self.map[x])
+                buffer[y] = agent
+                buffer = "".join(buffer)
+                self.map[x] = buffer
+
+
+        self.width = len(self.map[0])
+        self.height = len(self.map)
+
+    def get_info(self, x, y):
+        """
+            this method is used to get info of a specific grid on map
+            a map like:
+            ...
+            .#.
+            ##.
+            it's a list: ['...','.#.','##.'] in python
+            map[0][1] is .
+            map[1][1] is #
+        """
+        try:
+            char = self.map[x][y]
+        except IndexError:
+            return {}
+        try:
+            return self.key[char]
+        except KeyError:
+            return {}
+
+    def is_enamy(self, x, y, camp):
+        """
+            this method is used to check whether there is an enamy
+            on a specific grid.
+        """
+        enamy_map = {
+            'human' : 'computer',
+            'computer' : 'human'
+        }
+        grid_info = self.get_info(x, y)
+        try:
+            return grid_info['camp'] == enamy_map[camp]
+        except KeyError:
+            return False
+
+    def is_teammate(self, x, y, camp):
+        """
+            this method is used to check whether there is an enamy
+            on a specific grid.
+        """
+        grid_info = self.get_info(x, y)
+        try:
+            return grid_info['camp'] == camp
+        except KeyError:
+            return False
